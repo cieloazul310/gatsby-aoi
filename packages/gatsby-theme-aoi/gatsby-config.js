514e866680084a0a10d69ae19b0bef48caceca88
cieloazul310 <cieloazul310@gmail.com>
2022-01-22 15:50:12 +0900

    update

--- packages/gatsby-theme-aoi/gatsby-config.js
+++ packages/gatsby-theme-aoi/gatsby-config.js
@@ -1,5 +1,35 @@
-module.exports = {
+module.exports = (options) => ({
+  siteMetadata: {
+    title: `Gatsby Theme Aoi`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@cieloazul310`,
+    siteUrl: '',
+    baseUrl: '',
+    keywords: ['Gatsby', 'TypeScript', 'Material-UI'],
+    lang: '',
+    social: {
+      mail: '',
+      twitter: '',
+      github: '',
+      facebook: '',
+      gitlab: '',
+      linkedin: '',
+      medium: '',
+      pocket: '',
+      tumblr: '',
+      instagram: '',
+      vimeo: '',
+      youtube: '',
+    },
+  },
   plugins: [
-    
+    {
+      resolve: `@cieloazul310/gatsby-theme-aoi-top-layout`,
+      options: {
+        siteId: options.siteId,
+      },
+    },
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-react-helmet`,
   ],
-}
+});
--- packages/gatsby-theme-aoi/src/index.js
+++
@@ -1,4 +0,0 @@
-export default function gatsbyThemeAoi() {
-  // eslint-disable-next-line no-console
-  console.log('Hello, gatsby-theme-aoi!');
-}
