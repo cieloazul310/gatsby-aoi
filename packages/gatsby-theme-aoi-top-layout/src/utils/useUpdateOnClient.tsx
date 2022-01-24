0cfaafae6f457c990ad0026ce4159abde522429f
cieloazul310 <cieloazul310@gmail.com>
2022-01-24 10:00:56 +0900

    update

---
+++ packages/gatsby-theme-aoi-top-layout/src/utils/useUpdateOnClient.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+/**
+ * usage:
+ * `<div key={isClient}>`
+ */
+export default function useUpdateOnClient() {
+  const [isClient, setIsClient] = React.useState(false);
+  React.useEffect(() => {
+    setIsClient(true);
+  }, []);
+  return `${isClient}`;
+}
