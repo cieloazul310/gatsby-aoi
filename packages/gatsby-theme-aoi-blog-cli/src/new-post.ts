82ba018ea44f5a75ded758e481448d85e122da39
cieloazul310 <cieloazul310@gmail.com>
2022-02-07 17:38:32 +0900

    feat(blog-cli)!: Enable to use archetypes

--- packages/gatsby-theme-aoi-blog-cli/src/new-post.ts
+++ packages/gatsby-theme-aoi-blog-cli/src/new-post.ts
@@ -1,6 +1,6 @@
-/* eslint no-console: off */
 import * as fs from 'fs';
 import * as path from 'path';
+import findArchetypes from './findArchetypes';
 import archetypesToMd from './archetypeToMd';
 import { isString, isBoolean } from './utils';
 
@@ -29,42 +29,51 @@ export default function newPost(title: string, options: NewPostOptions) {
   const pwd = process.env.PWD;
   if (!pwd) throw new Error();
   const { author, date, canonical, mdx } = options;
-  console.log(author, date, canonical, mdx);
-  console.log(__dirname);
-  const archetypesPath = path.resolve(__dirname, '../archetypes.md');
 
+  /**
+   * @todo
+   * contentDir should be configured in the root gatsby project.
+   */
   const baseDir = '/content/posts/';
   const datetime = parseDate(date);
   const dateDir = dateToDirname(datetime);
   const titleSlug = strToSlug(title);
+
   // /content/posts/2022/02
-  let dir = path.join(pwd, baseDir, dateDir);
+  const dir = path.join(pwd, baseDir, dateDir);
+  // /content/posts/2022/02/${slug}
+  const slugDir = path.join(dir, titleSlug);
+  const isCanonical = isBoolean(canonical) && canonical;
+
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
   }
-  if (isBoolean(canonical) && canonical) {
-    // /content/posts/2022/02/titleSlug
-    dir = path.join(dir, titleSlug);
-    if (fs.existsSync(dir)) {
-      throw new Error(`${dir} already exists!`);
-    }
-    fs.mkdirSync(dir);
+  if (
+    fs.existsSync(path.resolve(dir, `${titleSlug}.md`)) ||
+    fs.existsSync(path.resolve(dir, `${titleSlug}.mdx`)) ||
+    fs.existsSync(slugDir)
+  ) {
+    throw new Error(`${titleSlug} is already exists!`);
   }
-
-  const name = isBoolean(canonical) && canonical ? 'index' : titleSlug;
+  if (isCanonical) {
+    fs.mkdirSync(slugDir);
+  }
+  const name = isCanonical ? 'index' : titleSlug;
   const extension = isBoolean(mdx) && mdx ? 'mdx' : 'md';
   const filename = `${name}.${extension}`;
-  const filePath = path.resolve(dir, filename);
+  const filePath = path.resolve(isCanonical ? slugDir : dir, filename);
 
   if (fs.existsSync(filePath)) {
     throw new Error(`${filePath} already exists!`);
   }
-  const md = archetypesToMd(archetypesPath, {
+  const archetypePath = findArchetypes(pwd);
+  const md = archetypesToMd(archetypePath, {
     title,
     date: datetime,
     author: isString(author) ? author : 'Unknown author',
   });
 
   fs.writeFileSync(filePath, md);
+  /* eslint-disable-next-line no-console */
   console.log(`Generate ${path.resolve(dir, filename)}!`);
 }
