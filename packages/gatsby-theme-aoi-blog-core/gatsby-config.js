69eea9328c26bd10c9dd07aec7a29b11012d84ed
cieloazul310 <cieloazul310@gmail.com>
2022-02-10 15:01:21 +0900

    update

--- packages/gatsby-theme-aoi-blog-core/gatsby-config.js
+++ packages/gatsby-theme-aoi-blog-core/gatsby-config.js
@@ -7,24 +7,6 @@ module.exports = (themeOptions) => {
   const options = withDefaults(themeOptions);
 
   return {
-    siteMetadata: {
-      title: `Gatsby Theme Aoi Blog Core`,
-      description: `Gatsby Theme Aoi Blog Core package.`,
-      author: `@cieloazul310`,
-      siteUrl: 'https://gatsby-theme-aoi.netlify.app',
-      keywords: ['Gatsby', 'TypeScript', 'MUI'],
-      lang: 'en',
-      social: [
-        {
-          name: 'twitter',
-          url: 'https://twitter.com/cieloazul310',
-        },
-        {
-          name: 'github',
-          url: 'https://github.com/cieloazul310',
-        },
-      ],
-    },
     plugins: [
       {
         resolve: `gatsby-source-filesystem`,
--- packages/gatsby-theme-aoi-blog/gatsby-config.js
+++ packages/gatsby-theme-aoi-blog/gatsby-config.js
@@ -1,4 +1,22 @@
 module.exports = (options) => ({
+  siteMetadata: {
+    title: `Gatsby Theme Aoi Blog Core`,
+    description: `Gatsby Theme Aoi Blog Core package.`,
+    author: `@cieloazul310`,
+    siteUrl: 'https://gatsby-theme-aoi.netlify.app',
+    keywords: ['Gatsby', 'TypeScript', 'MUI'],
+    lang: 'en',
+    social: [
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/cieloazul310',
+      },
+      {
+        name: 'github',
+        url: 'https://github.com/cieloazul310',
+      },
+    ],
+  },
   plugins: [
     `@cieloazul310/gatsby-theme-aoi-blog-templates`,
     {
