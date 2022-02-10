8368a6aad98ff6a9b5d69d018ca3a40f7c0fd98e
cieloazul310 <cieloazul310@gmail.com>
2022-02-10 17:31:31 +0900

    fix(blog-utils): fix validUrl

--- starters/gatsby-starter-aoi-blog-theme/gatsby-config.js
+++ starters/gatsby-starter-aoi-blog-theme/gatsby-config.js
@@ -47,45 +47,33 @@ module.exports = {
     {
       resolve: `@cieloazul310/gatsby-theme-aoi-blog`,
       options: {
-        assetPath: `content/images`,
+        // postsPerPage: 15,
+        // contentPath: `content`,
+        /*
+        basePaths: {
+          posts: '/posts',
+          category: '/category',
+          tag: '/tag',
+          author: '/author',
+        },
+        */
+        // assetPath: `content/assets`,
+        // excerptLength: 140,
+        imageMaxWidth: 800,
         gatsbyRemarkPlugins: [
           `gatsby-remark-embed-snippet`,
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
               inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
               aliases: {},
-              // This toggles the display of line numbers globally alongside the code.
-              // To use it, add the following line in gatsby-browser.js
-              // right after importing the prism color scheme:
-              //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-              // Defaults to false.
-              // If you wish to only show line numbers on certain code blocks,
-              // leave false and use the {numberLines: true} syntax below
               showLineNumbers: false,
-              // If setting this to true, the parser won't handle and highlight inline
-              // code used in markdown i.e. single backtick code like `this`.
               noInlineHighlight: true,
-              // Customize the prompt used in shell output
-              // Values below are default
               prompt: {
                 user: 'root',
                 host: 'localhost',
                 global: false,
               },
-              // By default the HTML entities <>&'" are escaped.
-              // Add additional HTML escapes by providing a mapping
-              // of HTML entities and their escape value IE: { '}': '&#123;' }
               escapeEntities: {},
             },
           },
