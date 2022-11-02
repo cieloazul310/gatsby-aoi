a51721519f0200e3447a72da81390449f39ad5b6
cieloazul310 <cieloazul310@gmail.com>
2022-11-02 21:01:16 +0900

    chore(components): storybook

--- packages/gatsby-theme-aoi-components/.storybook/main.js
+++ packages/gatsby-theme-aoi-components/.storybook/main.js
@@ -4,7 +4,14 @@ const toPath = (filePath) => path.join(process.cwd(), filePath);
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
   webpackFinal: async (config) => {
     return {
       ...config,
